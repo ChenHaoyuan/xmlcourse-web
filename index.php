1786d3d6023b05f7a3eb26c4fb75527c7a11fd9d
陈浩远 <chenhaoyuan@mac-mini.local>
2016-03-25 16:12:24 +0800

    change index page to php

---
+++ index.php
@@ -0,0 +1,237 @@
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+	<title>RT Triangle</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<!--[if lte IE 8]>
+	<script src="assets/js/ie/html5shiv.js"></script>
+	<![endif]-->
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<!--[if lte IE 9]>
+	<link rel="stylesheet" href="assets/css/ie9.css" />
+	<![endif]-->
+	<!--[if lte IE 8]>
+	<link rel="stylesheet" href="assets/css/ie8.css" />
+	<![endif]-->
+</head>
+<body>
+
+	<!-- Sidebar -->
+	<section id="sidebar">
+		<div class="inner">
+			<nav>
+				<ul>
+					<li>
+						<a href="#intro">Welcome</a>
+					</li>
+					<li>
+						<a href="#one">Our Team</a>
+					</li>
+					<li>
+						<a href="#two">Our Work</a>
+					</li>
+					<li>
+						<a href="#three">Our schedule</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</section>
+
+	<!-- Wrapper -->
+	<div id="wrapper">
+
+		<!-- Intro -->
+		<section id="intro" class="wrapper style1 fullscreen fade-up">
+			<div class="inner">
+				<h1>RT Triangle</h1>
+				<p>
+					Our group is seventh group in XML course and our web is in the
+					<a href="https://github.com/ChenHaoyuan/xmlcourse-web">github</a>
+					.
+				</p>
+				<p>This web is display XML course work.</p>
+				<p>Our data source is picture</p>
+				<ul class="actions">
+					<li>
+						<a href="#one" class="button scrolly">Learn more</a>
+					</li>
+				</ul>
+			</div>
+		</section>
+
+		<!-- One -->
+		<section id="one" class="wrapper style2 spotlights">
+			<section>
+				<a href="#" class="image">
+					<img src="images/icon.png" alt="" data-position="top center" />
+				</a>
+				<div class="content">
+					<div class="inner">
+						<h2>Group name:RT Triangle</h2>
+						<p>
+							Although our group name is RT Triangle,the icon is an equilateral triangle
+						</p>
+					</div>
+				</div>
+			</section>
+			<section>
+				<a href="#" class="image">
+					<img src="https://avatars2.githubusercontent.com/u/8957418?v=3&s=460" alt="" data-position="center center" />
+				</a>
+				<div class="content">
+					<div class="inner">
+						<h2>ChenHaoyuan</h2>
+						<div class="introduction_chen">
+							<p>student ID:71113333</p>
+							<p>email:chenhaoyuan@live.cn</p>
+							<p>QQ:827167063</p>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section>
+				<a href="#" class="image">
+					<img src="images/LiJie_head.jpg" alt="" data-position="top center" />
+				</a>
+				<div class="content">
+					<div class="inner">
+						<h2>LiJie</h2>
+						<p>student ID:71113134</p>
+						<p>QQ:1475563873</p>
+					</div>
+				</div>
+			</section>
+		</section>
+
+		<!-- Two -->
+		<section id="two" class="wrapper style3 fade-up">
+			<div class="inner">
+				<h2>Our work</h2>
+				<p>
+					You can get our work in the following or
+					<a href="https://github.com/ChenHaoyuan/xmlcourse">github</a>
+					.
+				</p>
+				<div class="features">
+					<section>
+						<span class="icon major fa-circle-blank"></span>
+						<h3>The first experiment:</h3>
+						<p>
+							experiment 0:
+							<a href="source/first_experiment/pictureLibraryDom.png">The picture library DOM</a>
+						</p>
+						<p>
+							experiment 1-1:
+							<a href="source/first_experiment/pictureLibrary.xml">The XML</a>
+						</p>
+						<p>
+							experiment 1-2:
+							<a href="source/first_experiment/pictureLibrary.dtd">The DTD</a>
+						</p>
+						<p>
+							experiment 1-3:
+							<a href="source/first_experiment/pictureLibrary.xsd">The Schema</a>
+						</p>
+					</section>
+					<section>
+						<span class="icon major fa-circle"></span>
+						<h3>The second experiment:</h3>
+						<p></p>
+					</section>
+					<section>
+						<span class="icon major fa-circle"></span>
+						<h3>The third experiment:</h3>
+						<p>Have not started.</p>
+					</section>
+					<section>
+						<span class="icon major fa-circle"></span>
+						<h3>The forth experiment:</h3>
+						<p>Have not started.</p>
+					</section>
+					<section>
+						<span class="icon major fa-circle"></span>
+						<h3>The fifth experiment:</h3>
+						<p>Have not started.</p>
+					</section>
+					<section>
+						<span class="icon major fa-circle"></span>
+						<h3>The sixth experiment:</h3>
+						<p>Have not started.</p>
+					</section>
+				</div>
+				<ul class="actions">
+					<li>
+						<a href="#" class="button">Learn more</a>
+					</li>
+				</ul>
+			</div>
+		</section>
+
+		<!-- Three -->
+		<section id="three" class="wrapper style1 fade-up">
+			<div class="inner">
+				<h2>Our schedule</h2>
+				<div class="split style1">
+					<section>
+						<h3>The first experiment:</h3>
+						<table border="1">
+							<tr>
+								<th>time</th>
+								<th>work</th>
+							</tr>
+							<tr>
+								<td>2016.3.1</td>
+								<td>Finish Site</td>
+							</tr>
+							<tr>
+								<td>2016.3.3</td>
+								<td>AssignmentOne</td>
+							</tr>
+							<tr>
+								<td>2016.3.7</td>
+								<td>AssignmentTwo</td>
+							</tr>
+							<tr>
+								<td>2016.3.10</td>
+								<td>AssignmentThree</td>
+							</tr>
+						</table>
+					</section>
+
+				</section>
+
+			</div>
+
+			<!-- Footer -->
+			<footer id="footer" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						<li>&copy; Untitled. All rights reserved.</li>
+						<li>
+							Design:
+							<a href="http://html5up.net">HTML5 UP</a>
+						</li>
+					</ul>
+				</div>
+			</footer>
+
+			<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]>
+			<script src="assets/js/ie/respond.min.js"></script>
+			<![endif]-->
+			<script src="assets/js/main.js"></script>
+
+</body>
+		</html>
---
+++ login.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+    <html lang="zh-CN">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1" />
+        <meta name="renderer" content="webkit" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>RT Triangle-login</title>
+        <meta name="description" content="login page" />
+        <!-- 样式文件 -->
+        <link rel="stylesheet" href="./../../ve_2_1/css/ve_wap.css" />
+    </head>
+
+    <body>
+
+       <section class=''>
+           <div class="container">
+               <div class="row">
+                 a
+               </div>
+           </div>
+       </section>
+
+       <!-- 尾部js -->
+       <script src="./../../ve_2_1/js/vendor/require/require.js"></script>
+       <script src="./../../ve_2_1/js/config.dev.js"></script>
+       <!-- 替换结束 -->
+
+       <!-- 自定义代码部分 -->
+      <script>
+       //js codes here.
+       require(['wap/index'], function() {
+           require(['wap/indexJs']);
+       });
+       </script>
+    </body>
+    </html>
